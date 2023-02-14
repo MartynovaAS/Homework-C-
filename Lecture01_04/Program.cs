e3f41a08d16ec000153e3bdacee5feab2286773e
Martynova <anastasiia.martynova.1993@mail.ru>
2023-02-14 12:26:17 +0300

    Задача номер 8 готова

---
+++ Lecture01_04/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+int p, n, index;
+
+index = 0;
+p = 1;
+n = 5;
+
+    while (index < 0)
+    {
+    while (p < n)
+    {
+        if (p % 2 > 0)
+        {
+         
+        }
+        else
+        {
+            Console.WriteLine(p);
+        }
+        p = p + 1;
+    }
+    index = index + 1;
+    Console.WriteLine("Hello");
+}   
