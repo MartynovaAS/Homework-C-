4c669edcfddbf27da3f7c7409d6346bc01a5cff9
Martynova <anastasiia.martynova.1993@mail.ru>
2023-02-18 14:38:04 +0300

    Задача № 10

---
+++ Lecture02_02/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+//что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+Console.WriteLine("Введите любое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99)
+{
+    while (number > 999)
+    {
+        number = (int) ((double)number / 10);
+    }
+    number = number % 10;
+    {
+        Console.Write("Третья цифра: ");
+        Console.WriteLine(number);
+    }
+ }
+ else 
+ {
+   Console.WriteLine("Третьей цифры в этом числе нет");
+ }
+
