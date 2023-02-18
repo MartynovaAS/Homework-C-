1ed6498e5f42422999ff5face862757861608873
Martynova <anastasiia.martynova.1993@mail.ru>
2023-02-18 14:55:45 +0300

    Задача готова

---
+++ Lecture02_01/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, 
+// которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int value = 16;
+if (value > 99 || value >= 1000)
+{
+int result = (value / 10) % 10;
+Console.WriteLine(result);
+result = (value / 10) - ((value / 10) / 10 * 10);
+Console.WriteLine(result);
+}
+else{
+  Console.WriteLine("Число неверное"); 
+}
