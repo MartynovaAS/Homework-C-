53c7e6e17f4f200b2ee410b8baf94ad4bb089152
Martynova <anastasiia.martynova.1993@mail.ru>
2023-02-27 17:33:02 +0300

    Задача номер 25

---
+++ Lecture04_1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите метод, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+ int a = 0;
+ int b = 0;
+Method(a,b);
+
+void Method(int a, int b)
+    {
+        Console.Write("Введите первое число: ");
+        int firstNumber = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите второе число: ");
+        int secondNumber = Convert.ToInt32(Console.ReadLine());
+        int method = firstNumber;
+        for (int i = 1; i < secondNumber; i++)
+        {
+            method = method * firstNumber;
+        }
+        Console.WriteLine($"{firstNumber} в степени {secondNumber} = " + method);
+    }
+
+
